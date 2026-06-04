e6b536d6cd91ed26639d02ee32c215174733e828
Abhay Kumar <abhaykumar.dev01@gmail.com>
2026-06-04 22:34:15 +0530

    Update Total Waviness of Numbers in Range I.cpp

--- Total Waviness of Numbers in Range I.cpp
+++ Total Waviness of Numbers in Range I.cpp
@@ -3,9 +3,7 @@ public:
     int waviness(int x) {
         string s = to_string(x);
         int n = s.size();
-
         if (n < 3) return 0;
-
         int cnt = 0;
 
         for (int i = 1; i < n - 1; i++) {
@@ -20,7 +18,6 @@ public:
 
     int totalWaviness(int num1, int num2) {
         int ans = 0;
-
         for (int num = num1; num <= num2; num++) {
             ans += waviness(num);
         }
