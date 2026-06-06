fe6333d02b5667a2e042226e84b63f16567f15dd
Abhay Kumar <abhaykumar.dev01@gmail.com>
2026-06-06 22:29:53 +0530

    Update Left and Right Sum Differences.cpp

--- arrays/Left and Right Sum Differences.cpp
+++ arrays/Left and Right Sum Differences.cpp
@@ -3,14 +3,11 @@ public:
     vector<int> leftRightDifference(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n);
-
         int total = 0;
         for (int x : nums) {
             total += x;
         }
-
         int leftSum = 0;
-
         for (int i = 0; i < n; i++) {
             int rightSum = total - leftSum - nums[i];
             ans[i] = abs(leftSum - rightSum);
