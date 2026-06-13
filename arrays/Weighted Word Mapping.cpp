080d3c25c5769a1c29f5745b2e832eddb92c2360
Abhay Kumar <abhaykumar.dev01@gmail.com>
2026-06-13 10:31:04 +0530

    Create Weighted Word Mapping.cpp

---
+++ arrays/Weighted Word Mapping.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (string &word : words) {
+            long long sum = 0;
+
+            for (char c : word) {
+                sum += weights[c - 'a'];
+            }
+
+            ans.push_back('z' - (sum % 26));
+        }
+
+        return ans;
+     
+    }
+};
