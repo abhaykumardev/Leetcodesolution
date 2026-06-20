fd8f5a6a84ca701ae1881c82028731350f1a5acf
Abhay Kumar <abhaykumar.dev01@gmail.com>
2026-06-20 08:07:24 +0530

    Update ind the Highest Altitude.cpp

--- arrays/ind the Highest Altitude.cpp
+++ arrays/ind the Highest Altitude.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curraltitude = 0;
+        int maxaltitude = 0;
+
+        for(int g: gain){
+            curraltitude += g;
+            maxaltitude = max(maxaltitude, curraltitude);
+        }
+        return maxaltitude;
+    }
+};
+
+//leetcode 1732
