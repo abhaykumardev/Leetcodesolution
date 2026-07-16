4100da96a7a6142b4956847ca1a5ad76712166c1
Abhay Kumar <abhaykumar.dev01@gmail.com>
2026-07-16 09:22:38 +0530

    Update Sum of GCD of formed pairs.cpp

--- arrays/Sum of GCD of formed pairs.cpp
+++ arrays/Sum of GCD of formed pairs.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefixGcd;
-        
+        vector<int> prefixGcd;    
         int mx = 0;
-
         // Construct prefixGcd
         for (int num : nums) {
             mx = max(mx, num);
             prefixGcd.push_back(__gcd(num, mx));
         }
-
         // Sort the array
         sort(prefixGcd.begin(), prefixGcd.end());
-
         long long ans = 0;
         int left = 0, right = n - 1;
-
         // Pair smallest with largest
         while (left < right) {
             ans += __gcd(prefixGcd[left], prefixGcd[right]);
@@ -28,3 +23,6 @@ public:
         return ans;
     }
 };
+
+
+// leetcode 3867
