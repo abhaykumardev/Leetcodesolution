02a828805c4b583dec8e9a23be0e2158b2c5b9cb
Abhay Kumar <abhaykumar.dev01@gmail.com>
2026-07-18 10:50:35 +0530

    Update find the greatest common divisor of array.cpp

--- arrays/find the greatest common divisor of array.cpp
+++ arrays/find the greatest common divisor of array.cpp
@@ -3,13 +3,11 @@ public:
     int findGCD(vector<int>& nums) {
         int mn = nums[0];
         int mx = nums[0];
-
         // Find minimum and maximum element
         for (int num : nums) {
             mn = min(mn, num);
             mx = max(mx, num);
         }
-
         // Find GCD using Euclidean Algorithm
         while (mx != 0) {
             int rem = mn % mx;
@@ -20,3 +18,5 @@ public:
         return mn;
     }
 };
+
+// leetcode 1979
