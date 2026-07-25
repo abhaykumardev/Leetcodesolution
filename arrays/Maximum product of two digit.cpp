9ac8b5b1013f9ce7a2c4bbd37eb8dc32a8163f29
Abhay Kumar <abhaykumar.dev01@gmail.com>
2026-07-25 19:53:49 +0530

    Update Maximum product of two digit.cpp

--- arrays/Maximum product of two digit.cpp
+++ arrays/Maximum product of two digit.cpp
@@ -5,17 +5,17 @@ public:
 
         while (n > 0) {
             int digit = n % 10;
-
             if (digit >= first) {
                 second = first;
                 first = digit;
             } else if (digit > second) {
                 second = digit;
             }
-
             n /= 10;
         }
-
         return first * second;
     }
 };
+
+
+// leetcode 3536
